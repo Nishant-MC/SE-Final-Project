c0641f9eef3d5c372ba046a003930bab2e938b36
JinU <jinu@gmail.com>
2014-04-24 17:40:02 +0400

    Very simple sign up and aboutus page, tutorial point upto 12

---
+++ ROR/src/signups/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+
+#message handler
+from django.contrib import messages
+
+from .forms import SignUpForm
+# Create your views here.
+
+
+def home(request):
+    
+    form = SignUpForm(request.POST or None)
+    
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        save_it.save()
+        messages.success(request, 'Thank you for signing up!')
+        return HttpResponseRedirect('/thank-you/')
+        
+    return render_to_response("signup.html",
+                              locals(),
+                              context_instance=RequestContext(request)
+                              )
+
+def thankyou(request):
+            
+    return render_to_response("thankyou.html",
+                              locals(),
+                              context_instance=RequestContext(request)
+                              )
+
+def aboutus(request):
+            
+    return render_to_response("aboutus.html",
+                              locals(),
+                              context_instance=RequestContext(request)
+                              )
